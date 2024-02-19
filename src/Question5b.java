db6b93622cd9b15941d5094ace470a93d0532cb1
Ashmita124 <126133726+Ashmita124@users.noreply.github.com>
2024-02-19 22:46:08 +0545

    Question 5b
    Assume you were hired to create an application for an ISP, and there are n network devices, such as routers,
    that are linked together to provide internet access to users. You are given a 2D array that represents network
    connections between these network devices. write an algorithm to return impacted network devices, If there is
    a power outage on a certain device, these impacted device list assist you notify linked consumers that there is a
    power outage and it will take some time to rectify an issue.
    Input: edges= {{0,1},{0,2},{1,3},{1,6},{2,4},{4,6},{4,5},{5,7}}
    Target Device (On which power Failure occurred): 4
    Output (Impacted Device List) = {5,7}

--- src/Question5b.java
+++ src/Question5b.java
@@ -1,45 +1,95 @@
 import java.util.*;
 
 public class Question5b {
+    int[] discoveryTimes, lowValues;
+    int time = 1;
+    List<List<Integer>> answer = new ArrayList<>();
+    Map<Integer, List<Integer>> edgeMapping = new HashMap<>();
 
-    public static List<Integer> findImpactedDevices(int[][] edges, int targetDevice) {
-        Map<Integer, List<Integer>> graph = buildGraph(edges);
-        Set<Integer> visited = new HashSet<>();
-        List<Integer> impactedDevices = new ArrayList<>();
-        dfs(graph, targetDevice, visited, impactedDevices);
-        return impactedDevices;
+    public List<Integer> findImpactedDevices(int n, List<List<Integer>> connections, int targetDevice) {
+        discoveryTimes = new int[n];
+        lowValues = new int[n];
+        for (int i = 0; i < n; i++)
+            edgeMapping.put(i, new ArrayList<Integer>());
+        for (List<Integer> conn : connections) {
+            edgeMapping.get(conn.get(0)).add(conn.get(1));
+            edgeMapping.get(conn.get(1)).add(conn.get(0));
+        }
+        dfs(targetDevice, -1);
+
+        // Check if the target device is a source node in any connection
+        boolean isSourceNode = false;
+        for (List<Integer> conn : connections) {
+            if (conn.get(0) == targetDevice) {
+                isSourceNode = true;
+                break;
+            }
+        }
+
+        if (!isSourceNode) {
+            return new ArrayList<>();
+        }
+
+        Set<Integer> impactedDevicesSet = new HashSet<>();
+        for (List<Integer> connection : answer) {
+            int u = connection.get(0);
+            int v = connection.get(1);
+
+            if (u == targetDevice) {
+                impactedDevicesSet.add(v);
+            } else if (v == targetDevice) {
+                impactedDevicesSet.add(u);
+            }
+        }
+
+        Set<Integer> additionalAffectedDevices = new HashSet<>();
+        for (int affectedDevice : impactedDevicesSet) {
+            for (int neighbor : edgeMapping.get(affectedDevice)) {
+                if (!impactedDevicesSet.contains(neighbor)) {
+                    additionalAffectedDevices.add(neighbor);
+                }
+            }
+        }
+
+        impactedDevicesSet.addAll(additionalAffectedDevices);
+        impactedDevicesSet.remove(targetDevice);
+
+        return new ArrayList<>(impactedDevicesSet);
     }
 
-    private static void dfs(Map<Integer, List<Integer>> graph, int currentDevice,
-                            Set<Integer> visited, List<Integer> impactedDevices) {
-        visited.add(currentDevice);
-        if (graph.containsKey(currentDevice)) {
-            for (int neighbor : graph.get(currentDevice)) {
-                if (!visited.contains(neighbor)) {
-                    if (neighbor != currentDevice) { // Avoid self-loop
-                        impactedDevices.add(neighbor);
-                    }
-                    dfs(graph, neighbor, visited, impactedDevices);
-                }
+    public void dfs(int current, int previous) {
+        discoveryTimes[current] = lowValues[current] = time++;
+        for (int next : edgeMapping.get(current)) {
+            if (next == previous) continue;
+            if (discoveryTimes[next] == 0) {
+                dfs(next, current);
+                lowValues[current] = Math.min(lowValues[current], lowValues[next]);
+                if (lowValues[next] > discoveryTimes[current])
+                    answer.add(Arrays.asList(current, next));
+            } else {
+                lowValues[current] = Math.min(lowValues[current], discoveryTimes[next]);
             }
         }
     }
 
-    private static Map<Integer, List<Integer>> buildGraph(int[][] edges) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] edge : edges) {
-            int src = edge[0];
-            int dest = edge[1];
-            graph.computeIfAbsent(src, k -> new ArrayList<>()).add(dest);
-            graph.computeIfAbsent(dest, k -> new ArrayList<>()).add(src);
-        }
-        return graph;
-    }
-
     public static void main(String[] args) {
-        int[][] edges = {{0,1},{0,2},{1,3},{1,6},{2,4},{4,6},{4,5},{5,7}};
+        Question5b q5b = new Question5b();
+
+        int n = 8;
+        List<List<Integer>> connections = new ArrayList<>();
+        connections.add(Arrays.asList(0, 1));
+        connections.add(Arrays.asList(0, 2));
+        connections.add(Arrays.asList(1, 3));
+        connections.add(Arrays.asList(1, 6));
+        connections.add(Arrays.asList(2, 4));
+        connections.add(Arrays.asList(4, 6));
+        connections.add(Arrays.asList(4, 5));
+        connections.add(Arrays.asList(5, 7));
+
         int targetDevice = 4;
-        List<Integer> impactedDevices = findImpactedDevices(edges, targetDevice);
-        System.out.println("Impacted Device List: " + impactedDevices);
+
+        List<Integer> impactedDevices = q5b.findImpactedDevices(n, connections, targetDevice);
+
+        System.out.println(impactedDevices);
     }
 }
